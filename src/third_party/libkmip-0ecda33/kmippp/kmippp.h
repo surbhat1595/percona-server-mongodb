f6dac18080e977df552c818d81a307666a1ad20f
Konstantin Trushin <konstantin.trushin@percona.com>
2022-05-20 16:13:36 +0300

    PSMDB-1063: kmippp::context: eliminate unnecessary string copying

--- src/third_party/libkmip-0ecda33/kmippp/kmippp.h
+++ src/third_party/libkmip-0ecda33/kmippp/kmippp.h
@@ -1,6 +1,3 @@
-
-
-
 #include <memory>
 #include <cstdint>
 #include <vector>
@@ -12,19 +9,17 @@ typedef struct bio_st BIO;
 }
 
 namespace kmippp {
-  
   class context {
     public:
-
       using key_t = std::vector<unsigned char>;
       using id_t = std::string;
       using ids_t = std::vector<std::string>;
       using name_t = std::string;
 
-      context(std::string server_address,
-              std::string server_port,
-              std::string client_cert_fn,
-              std::string ca_cert_fn);
+      context(const std::string& server_address,
+              const std::string& server_port,
+              const std::string& client_cert_fn,
+              const std::string& ca_cert_fn);
       ~context();
 
       context(context &&) noexcept = default;
@@ -34,24 +29,24 @@ namespace kmippp {
       context& operator=(context const&) = delete;
 
       // KMIP::create operation, generates a new AES symmetric key on the server
-      id_t op_create(name_t name, name_t group);
+      id_t op_create(const name_t& name, const name_t& group);
 
       // KMIP::register operation, stores an existing symmetric key on the server
-      id_t op_register(name_t name, name_t group, key_t k);
+      id_t op_register(const name_t& name, const name_t& group, const key_t& k);
 
       // KMIP::get operation, retrieve a symmetric key by id
-      key_t op_get(id_t id);
+      key_t op_get(const id_t& id);
 
       // KMIP::get_attribute operation, retrieve the name of a symmetric key by id
-      name_t op_get_name_attr(id_t id);
+      name_t op_get_name_attr(const id_t& id);
 
       // KMIP::locate operation, retrieve symmetric keys by name
       // note: name can be empty, and will retrieve all keys
-      ids_t op_locate(name_t name);
+      ids_t op_locate(const name_t& name);
 
-      ids_t op_locate_by_group(name_t group);
+      ids_t op_locate_by_group(const name_t& group);
 
-      bool op_destroy(id_t id);
+      bool op_destroy(const id_t& id);
 
       // KMIP::locate operation, retrieve all symmetric keys
       // note: name can be empty, and will retrieve all keys
@@ -60,7 +55,5 @@ namespace kmippp {
     private:
       SSL_CTX *ctx_ = nullptr;
       BIO* bio_;
-      uint8_t* encoding;
   };
-
 }

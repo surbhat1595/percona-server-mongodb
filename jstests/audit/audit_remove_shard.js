7f513059fc22956b5e32672c78f82dc9c4f76d6e
Igor Solodovnikov <igor.solodovnikov@percona.com>
2022-07-26 17:23:56 +0300

    PSMDB-1104 fix three tests in 'audit' suite

--- jstests/audit/audit_remove_shard.js
+++ jstests/audit/audit_remove_shard.js
@@ -9,22 +9,33 @@ if (TestData.testData !== undefined) {
 auditTestShard(
     'removeShard',
     function(st) {
+        let rsname = "testreplset";
         var port = allocatePorts(10)[9];
-        var conn1 = MongoRunner.runMongod({dbpath: '/data/db/' + jsTestName() + '-extraShard-' + port, port: port,  shardsvr: ""});
+        var conn1 = MongoRunner.runMongod({dbpath: '/data/db/' + jsTestName() + '-extraShard-' + port, port: port,  shardsvr: "", replSet: rsname});
 
         var hostandport = conn1.host;
-        assert.commandWorked(st.s0.adminCommand({addshard: hostandport, name: 'removable'}));
+        assert.commandWorked(conn1.adminCommand({
+            replSetInitiate: {
+                "_id": rsname,
+                "members": [{"_id": 0, "host": hostandport}]
+            }
+        }));
 
+        let connstr = rsname + "/" + hostandport;
+        assert.commandWorked(st.s0.adminCommand({addshard: connstr, name: 'removable'}));
+
+        let cmdcnt = 0;
         const beforeCmd = Date.now();
         var removeRet;
         do {
+            ++cmdcnt;
             removeRet = st.s0.adminCommand({removeShard: 'removable'});
             assert.commandWorked(removeRet);
         } while (removeRet.state != 'completed');
 
         const beforeLoad = Date.now();
         auditColl = loadAuditEventsIntoCollection(st.s0, getDBPath() + '/auditLog-c0.json', jsTestName(), 'auditEvents');
-        assert.eq(1, auditColl.count({
+        assert.eq(cmdcnt, auditColl.count({
             atype: "removeShard",
             ts: withinInterval(beforeCmd, beforeLoad),
             'param.shard': 'removable',

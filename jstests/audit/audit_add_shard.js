8597a6a1c2e92cf5ca8d199c173a490fe17e2a09
Igor Solodovnikov <igor.solodovnikov@percona.com>
2022-07-26 17:23:56 +0300

    PSMDB-1104 fix three tests in 'audit' suite

--- jstests/audit/audit_add_shard.js
+++ jstests/audit/audit_add_shard.js
@@ -9,19 +9,27 @@ if (TestData.testData !== undefined) {
 auditTestShard(
     'addShard',
     function(st) {
+        let rsname = "testreplset";
         var port = allocatePorts(10)[9];
-        var conn1 = MongoRunner.runMongod({dbpath: '/data/db/' + jsTestName() + '-extraShard-' + port, port: port,  shardsvr: ""});
-
+        var conn1 = MongoRunner.runMongod({dbpath: '/data/db/' + jsTestName() + '-extraShard-' + port, port: port,  shardsvr: "", replSet: rsname});
         var hostandport = conn1.host;
+        assert.commandWorked(conn1.adminCommand({
+            replSetInitiate: {
+                "_id": rsname,
+                "members": [{"_id": 0, "host": hostandport}]
+            }
+        }));
+
+        let connstr = rsname + "/" + hostandport;
         const beforeCmd = Date.now();
-        assert.commandWorked(st.s0.adminCommand({addshard: hostandport}));
+        assert.commandWorked(st.s0.adminCommand({addshard: connstr}));
 
         const beforeLoad = Date.now();
         auditColl = loadAuditEventsIntoCollection(st.s0, getDBPath() + '/auditLog-s0.json', jsTestName(), 'auditEvents');
         assert.eq(1, auditColl.count({
             atype: "addShard",
             ts: withinInterval(beforeCmd, beforeLoad),
-            'param.connectionString': hostandport,
+            'param.connectionString': connstr,
             result: 0,
         }), "FAILED, audit log: " + tojson(auditColl.find().toArray()));
 

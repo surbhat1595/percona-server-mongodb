5fbd93a735c02078328a6c12c576c65e123370f9
Vadim Yalovets <vadim.yalovets@percona.com>
2022-08-04 17:00:52 +0300

    PSMDB-1094 v4.4 Remove jemalloc tag from build pipeline

--- percona-packaging/scripts/psmdb_builder.sh
+++ percona-packaging/scripts/psmdb_builder.sh
@@ -104,6 +104,8 @@ get_sources(){
         return 0
     fi
     PRODUCT=percona-server-mongodb
+    JEMALLOC_TAG=psmdb-3.2.11-3.1
+
     echo "PRODUCT=${PRODUCT}" > percona-server-mongodb-44.properties
     echo "PSM_BRANCH=${PSM_BRANCH}" >> percona-server-mongodb-44.properties
     echo "JEMALLOC_TAG=${JEMALLOC_TAG}" >> percona-server-mongodb-44.properties

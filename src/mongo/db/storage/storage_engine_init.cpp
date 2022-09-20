dd9d55b44cd3aed7201085e65ca8c51dbc170b2e
Konstantin Trushin <konstantin.trushin@percona.com>
2022-09-20 13:01:21 +0400

    PSMDB-1116: Correctly exit mongod after master key rotation
    Use proper severity for logging successful master key rotation and
    finish mongod process with zero exit code.

--- src/mongo/db/storage/storage_engine_init.cpp
+++ src/mongo/db/storage/storage_engine_init.cpp
@@ -33,6 +33,7 @@
 
 #include "mongo/db/storage/storage_engine_init.h"
 
+#include <cstdlib>
 #include <map>
 #include <memory>
 
@@ -50,6 +51,7 @@
 #include "mongo/db/storage/storage_repair_observer.h"
 #include "mongo/logv2/log.h"
 #include "mongo/util/assert_util.h"
+#include "mongo/util/quick_exit.h"
 #include "mongo/util/str.h"
 
 namespace mongo {
@@ -216,7 +218,15 @@ StorageEngine::LastShutdownState initializeStorageEngine(OperationContext* opCtx
                       storageGlobalParams,
                       encryptionGlobalParams.kmipKeyIds.encryption,
                       initFlags);
-        throw;
+        if (!encryptionGlobalParams.kmipServerName.empty()) {
+            LOGV2(29111,
+                  "Rotated master encryption key",
+                  "oldMasterKeyId"_attr = encryptionGlobalParams.kmipKeyIds.decryption,
+                  "newMasterKeyId"_attr = encryptionGlobalParams.kmipKeyIds.encryption);
+        } else {
+            LOGV2(29111, "Rotated master encryption key");
+        }
+        quickExit(EXIT_SUCCESS);
     }
 
     if (lockFile) {

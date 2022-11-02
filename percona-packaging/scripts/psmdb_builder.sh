b91bff4d87ab4223cd48e489528cf061738cad15
Oleksandr Miroshnychenko <alex.miroshnychenko@percona.com>
2022-11-02 18:17:24 +0200

    PSMDB-1167 fix build for mongo-tools 100.6.0

--- percona-packaging/scripts/psmdb_builder.sh
+++ percona-packaging/scripts/psmdb_builder.sh
@@ -954,7 +954,7 @@ build_tarball(){
     cd mongo-tools
     . ./set_tools_revision.sh
     sed -i '12d' buildscript/build.go
-    sed -i '167,176d' buildscript/build.go
+    sed -i '168,177d' buildscript/build.go
     sed -i "s:versionStr,:\"$PSMDB_TOOLS_REVISION\",:" buildscript/build.go
     sed -i "s:gitCommit):\"$PSMDB_TOOLS_COMMIT_HASH\"):" buildscript/build.go
     ./make build

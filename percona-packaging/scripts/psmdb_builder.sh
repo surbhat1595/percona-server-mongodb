fb19cfc9829ea86e555e1349155083ae96b549d8
Oleksandr Miroshnychenko <alex.miroshnychenko@percona.com>
2022-12-06 17:43:27 +0200

    PSMDB-1161 Update text go module

--- percona-packaging/scripts/psmdb_builder.sh
+++ percona-packaging/scripts/psmdb_builder.sh
@@ -166,6 +166,15 @@ get_sources(){
     sed -i 's|GitCommit="$(git rev-parse HEAD)"|GitCommit="$PSMDB_TOOLS_COMMIT_HASH"|' set_goenv.sh
     echo "export PSMDB_TOOLS_COMMIT_HASH=\"$(git rev-parse HEAD)\"" > set_tools_revision.sh
     echo "export PSMDB_TOOLS_REVISION=\"${PSM_VER}-${PSM_RELEASE}\"" >> set_tools_revision.sh
+    export GOROOT="/usr/local/go/"
+    export GOPATH=$PWD/../
+    export PATH="/usr/local/go/bin:$PATH:$GOPATH"
+    export GOBINPATH="/usr/local/go/bin"
+    go mod edit \
+	    -replace golang.org/x/text@v0.3.0=golang.org/x/text@v0.3.8 \
+	    -replace golang.org/x/text@v0.3.7=golang.org/x/text@v0.3.8
+    go mod tidy
+    go mod vendor
     chmod +x set_tools_revision.sh
     cd ${WORKDIR}
     source percona-server-mongodb-50.properties

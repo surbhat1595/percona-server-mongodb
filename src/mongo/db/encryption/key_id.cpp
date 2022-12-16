55cac60d2628e0ca28ec8a8de7f9a505f82e2637
Konstantin Trushin <konstantin.trushin@percona.com>
2022-12-16 19:50:01 +0400

    PSMDB-1148 Implement encryption key identifiers

---
+++ src/mongo/db/encryption/key_id.cpp
@@ -0,0 +1,190 @@
+/*======
+This file is part of Percona Server for MongoDB.
+
+Copyright (C) 2022-present Percona and/or its affiliates. All rights reserved.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the Server Side Public License, version 1,
+    as published by MongoDB, Inc.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Server Side Public License for more details.
+
+    You should have received a copy of the Server Side Public License
+    along with this program. If not, see
+    <http://www.mongodb.com/licensing/server-side-public-license>.
+
+    As a special exception, the copyright holders give permission to link the
+    code of portions of this program with the OpenSSL library under certain
+    conditions as described in each individual source file and distribute
+    linked combinations including the program with the OpenSSL library. You
+    must comply with the Server Side Public License in all respects for
+    all of the code used other than as permitted herein. If you modify file(s)
+    with this exception, you may extend this exception to your version of the
+    file(s), but you are not obligated to do so. If you do not wish to do so,
+    delete this exception statement from your version. If you delete this
+    exception statement from all source files in the program, then also delete
+    it in the license file.
+======= */
+
+#include "mongo/db/encryption/key_id.h"
+
+#include <array>
+#include <utility>
+
+#include "mongo/base/string_data.h"
+#include "mongo/bson/bsonobj.h"
+#include "mongo/bson/bsonobjbuilder.h"
+#include "mongo/util/assert_util_core.h"
+#include "mongo/util/str.h"
+
+namespace mongo::encryption {
+namespace {
+template <typename DerivedProductType>
+struct KeyIdFactoryWrapper {
+    static std::unique_ptr<KeyId> create(const BSONObj& o) {
+        return DerivedProductType::create(o);
+    }
+};
+
+using FactoryFn = std::unique_ptr<KeyId> (*)(const BSONObj&);
+}  // namespace
+
+std::unique_ptr<KeyId> KeyId::fromStorageEngineEncryptionOptions(const BSONObj& options) {
+    static const std::array<std::pair<StringData, FactoryFn>, 2u> factories = {
+        {{StringData(VaultSecretId::_kSeeoFieldName), KeyIdFactoryWrapper<VaultSecretId>::create},
+         {StringData(KmipKeyId::_kSeeoFieldName), KeyIdFactoryWrapper<KmipKeyId>::create}}};
+
+    std::unique_ptr<KeyId> result;
+    for (const auto& elem : options) {
+        auto factory = std::find_if(
+            factories.begin(), factories.end(), [name = elem.fieldNameStringData()](const auto& f) {
+                return f.first == name;
+            });
+        if (factory == factories.end()) {
+            // Ignore an unknown filed names for the forward compatibility purpose.
+            continue;
+        }
+        if (result) {
+            throw std::runtime_error("more that one encryption method is specified");
+        }
+        if (elem.type() != BSONType::Object) {
+            throw std::runtime_error(str::stream()
+                                     << "'" << factory->first << "' is not a BSON object");
+        }
+        try {
+            result = factory->second(elem.Obj());
+        } catch (const std::runtime_error& e) {
+            throw std::runtime_error(str::stream()
+                                     << "invalid '" << factory->first << "': " << e.what());
+        }
+    }
+    return result;
+}
+
+std::unique_ptr<VaultSecretId> VaultSecretId::create(const BSONObj& o) {
+    BSONElement pathElem = o.getField("path");
+    if (pathElem.eoo()) {
+        throw std::runtime_error("no 'path' field");
+    }
+    if (pathElem.type() != BSONType::String) {
+        throw std::runtime_error("the 'path' field is not a string");
+    }
+
+    BSONElement versionElem = o.getField("version");
+    if (versionElem.eoo()) {
+        throw std::runtime_error("no 'version' field");
+    }
+    // Version is serialized as string to wark aroud BSON limitations.
+    // @see `VaultSecretId::_serializeImpl`.
+    if (versionElem.type() != BSONType::String) {
+        throw std::runtime_error("the 'version' field is not a string");
+    }
+    std::istringstream versionStr(versionElem.String());
+    std::uint64_t version = 0;
+    versionStr >> version;
+    if (version == 0) {
+        throw std::runtime_error(
+            "the 'version' field must be a positive "
+            "integer serialized as a string");
+    }
+    return std::make_unique<VaultSecretId>(pathElem.String(), version);
+}
+
+std::unique_ptr<KmipKeyId> KmipKeyId::create(const BSONObj& o) {
+    BSONElement keyIdElem = o.getField("keyId");
+    if (keyIdElem.eoo()) {
+        throw std::runtime_error("no 'keyId' field");
+    }
+    if (keyIdElem.type() != BSONType::String) {
+        throw std::runtime_error("the 'keyId' field is not a string");
+    }
+    return std::make_unique<KmipKeyId>(keyIdElem.String());
+}
+
+void KeyId::serializeToStorageEngineEncryptionOptions(BSONObjBuilder* b) const {
+    BSONObjBuilder sb = b->subobjStart(_seeoFieldName());
+    _serializeValueToSeeo(&sb);
+    sb.done();
+}
+
+const char* KeyFilePath::_seeoFieldName() const noexcept {
+    invariant(false &&
+              "An encryption key file path must be never serialized to storage engnie metadata.");
+    return nullptr;
+}
+
+void KeyFilePath::_serializeValueToSeeo(BSONObjBuilder* b) const {
+    invariant(false &&
+              "An encryption key file path must be never serialized to storage engnie metadata.");
+}
+
+void VaultSecretId::_serializeValueToSeeo(BSONObjBuilder* b) const {
+    _serializeImpl(b);
+}
+
+void KmipKeyId::_serializeValueToSeeo(BSONObjBuilder* b) const {
+    b->append("keyId", _keyId);
+}
+
+void KeyFilePath::serialize(BSONObjBuilder* b) const {
+    b->append("encryptionKeyFilePath", _path);
+}
+
+void VaultSecretId::serialize(BSONObjBuilder* b) const {
+    BSONObjBuilder sb = b->subobjStart("vaultSecretIdentifier");
+    _serializeImpl(&sb);
+    sb.done();
+}
+
+void VaultSecretId::_serializeImpl(BSONObjBuilder* b) const {
+    b->append("path", _path);
+    // serialize the version as a string to work around the BSON limitation
+    // on serializable types: only `int` or `long long` can be serialized as
+    // integer numbers
+    b->append("version", str::stream() << _version);
+}
+
+void KmipKeyId::serialize(BSONObjBuilder* b) const {
+    b->append("kmipKeyIdentifier", _keyId);
+}
+
+void KeyFilePath::accept(KeyIdConstVisitor& v) const {
+    v.visit(*this);
+}
+
+void VaultSecretId::accept(KeyIdConstVisitor& v) const {
+    v.visit(*this);
+}
+
+void KmipKeyId::accept(KeyIdConstVisitor& v) const {
+    v.visit(*this);
+}
+
+WtKeyIds& WtKeyIds::instance() {
+    static WtKeyIds ids;
+    return ids;
+}
+}  // namespace mongo::encryption
---
+++ src/mongo/db/encryption/key_id.h
@@ -0,0 +1,301 @@
+/*======
+This file is part of Percona Server for MongoDB.
+
+Copyright (C) 2022-present Percona and/or its affiliates. All rights reserved.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the Server Side Public License, version 1,
+    as published by MongoDB, Inc.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Server Side Public License for more details.
+
+    You should have received a copy of the Server Side Public License
+    along with this program. If not, see
+    <http://www.mongodb.com/licensing/server-side-public-license>.
+
+    As a special exception, the copyright holders give permission to link the
+    code of portions of this program with the OpenSSL library under certain
+    conditions as described in each individual source file and distribute
+    linked combinations including the program with the OpenSSL library. You
+    must comply with the Server Side Public License in all respects for
+    all of the code used other than as permitted herein. If you modify file(s)
+    with this exception, you may extend this exception to your version of the
+    file(s), but you are not obligated to do so. If you do not wish to do so,
+    delete this exception statement from your version. If you delete this
+    exception statement from all source files in the program, then also delete
+    it in the license file.
+======= */
+
+#pragma once
+
+#include <cstdint>
+#include <memory>
+#include <string>
+
+namespace mongo {
+class BSONObj;
+class BSONObjBuilder;
+class EncryptionGlobalParams;
+
+namespace encryption {
+class KeyIdConstVisitor;
+class ReadKey;
+
+class KeyId {
+public:
+    // @todo Consider introducing the macro which, given a class name, would
+    // declare all the six special member functions with default definitions,
+    // making the destructor virtual.
+    // Using the macro may eliminate boilerplate code.
+    // @see the `KeyIdConstVisitor` classes where
+    // boilerplate code is about half of the definition;
+    virtual ~KeyId() = default;
+    KeyId() = default;
+    KeyId(const KeyId&) = default;
+    KeyId(KeyId&&) = default;
+    KeyId& operator=(const KeyId&) = default;
+    KeyId& operator=(KeyId&&) = default;
+
+    virtual std::unique_ptr<KeyId> clone() const = 0;
+
+    /// @brief Reads an encryption key identifier (if any) from the storage
+    /// engine encryption options.
+    ///
+    /// @param options storage engine encryption options
+    ///
+    /// @returns pointer to the read key identifier, if any
+    ///
+    /// @throws std::runtime_error in case of invalid options format
+    static std::unique_ptr<KeyId> fromStorageEngineEncryptionOptions(const BSONObj& options);
+    /// @brief Serializes the encryption key identifier to the storage engine
+    /// encryption options, which, in turn, are kept in storage engine metadata.
+    void serializeToStorageEngineEncryptionOptions(BSONObjBuilder* b) const;
+    virtual bool needsSerializationToStorageEngineEncryptionOptions() const noexcept = 0;
+
+    /// @brief Makes the class and its subclasses loggable with LOGV2.
+    virtual void serialize(BSONObjBuilder* b) const = 0;
+
+    virtual void accept(KeyIdConstVisitor& v) const = 0;
+
+    /// @brief Returns the type of the key management facility suitable for
+    /// keeping the key this identifier points to.
+    virtual const char* facilityType() const noexcept = 0;
+
+private:
+    /// @brief Returns field name under which this key identifier is serialized
+    /// to the storage engine encryption opitons.
+    ///
+    /// @note sseo stands for storage engine encryption options.
+    virtual const char* _seeoFieldName() const noexcept = 0;
+    /// @breif Serializes the key identifier value itself to the storage engine
+    /// encrypiton opitons.
+    ///
+    /// @note sseo stands for storage engine encryption options.
+    /// @note This seiralization may differ from the serialization for logging.
+    virtual void _serializeValueToSeeo(BSONObjBuilder* b) const = 0;
+};
+
+class KeyFilePath : public KeyId {
+public:
+    explicit KeyFilePath(const std::string& path) : _path(path) {}
+    explicit KeyFilePath(std::string&& path) : _path(std::move(path)) {}
+    std::unique_ptr<KeyId> clone() const override {
+        return std::make_unique<KeyFilePath>(*this);
+    }
+
+    bool operator==(const KeyFilePath& that) const noexcept {
+        return _path == that._path;
+    }
+    bool operator!=(const KeyFilePath& that) const noexcept {
+        return !(*this == that);
+    }
+
+    const std::string& toString() const {
+        return _path;
+    }
+
+    bool needsSerializationToStorageEngineEncryptionOptions() const noexcept override {
+        return false;
+    }
+
+    void serialize(BSONObjBuilder* b) const override;
+    void accept(KeyIdConstVisitor& v) const override;
+
+    static constexpr const char* kFacilityType = "encryption key file";
+    const char* facilityType() const noexcept override {
+        return kFacilityType;
+    }
+
+private:
+    const char* _seeoFieldName() const noexcept override;
+    void _serializeValueToSeeo(BSONObjBuilder* b) const override;
+
+    std::string _path;
+};
+
+class VaultSecretId : public KeyId {
+public:
+    VaultSecretId(const std::string& path, std::uint64_t version)
+        : _path(path), _version(version) {}
+    VaultSecretId(std::string&& path, std::uint64_t version)
+        : _path(std::move(path)), _version(version) {}
+
+    static std::unique_ptr<VaultSecretId> create(const BSONObj& o);
+
+    std::unique_ptr<KeyId> clone() const override {
+        return std::make_unique<VaultSecretId>(*this);
+    }
+
+    bool operator==(const VaultSecretId& that) const noexcept {
+        return _path == that._path && _version == that._version;
+    }
+    bool operator!=(const VaultSecretId& that) const noexcept {
+        return !(*this == that);
+    }
+
+    const std::string& path() const noexcept {
+        return _path;
+    }
+    std::uint64_t version() const noexcept {
+        return _version;
+    }
+
+    bool needsSerializationToStorageEngineEncryptionOptions() const noexcept override {
+        return true;
+    }
+
+    void serialize(BSONObjBuilder* b) const override;
+    void accept(KeyIdConstVisitor& v) const override;
+
+    static constexpr const char* kFacilityType = "Vault server";
+    const char* facilityType() const noexcept override {
+        return kFacilityType;
+    }
+
+private:
+    friend std::unique_ptr<KeyId> KeyId::fromStorageEngineEncryptionOptions(const BSONObj& options);
+
+    static constexpr const char* _kSeeoFieldName = "vault";
+    const char* _seeoFieldName() const noexcept override {
+        return _kSeeoFieldName;
+    }
+    void _serializeValueToSeeo(BSONObjBuilder* b) const override;
+    void _serializeImpl(BSONObjBuilder* b) const;
+
+    std::string _path;
+    std::uint64_t _version;
+};
+
+class KmipKeyId : public KeyId {
+public:
+    explicit KmipKeyId(std::string&& keyId) : _keyId(std::move(keyId)) {}
+    explicit KmipKeyId(const std::string& keyId) : _keyId(keyId) {}
+
+    static std::unique_ptr<KmipKeyId> create(const BSONObj& o);
+
+    std::unique_ptr<KeyId> clone() const override {
+        return std::make_unique<KmipKeyId>(*this);
+    }
+
+    bool operator==(const KmipKeyId& that) const noexcept {
+        return _keyId == that._keyId;
+    }
+    bool operator!=(const KmipKeyId& that) const noexcept {
+        return !(*this == that);
+    }
+
+    const std::string& toString() const {
+        return _keyId;
+    }
+
+    bool needsSerializationToStorageEngineEncryptionOptions() const noexcept override {
+        return true;
+    }
+
+    void serialize(BSONObjBuilder* b) const override;
+    void accept(KeyIdConstVisitor& v) const override;
+
+    static constexpr const char* kFacilityType = "KMIP server";
+    const char* facilityType() const noexcept override {
+        return kFacilityType;
+    }
+
+private:
+    friend std::unique_ptr<KeyId> KeyId::fromStorageEngineEncryptionOptions(const BSONObj& options);
+
+    static constexpr const char* _kSeeoFieldName = "kmip";
+    const char* _seeoFieldName() const noexcept override {
+        return _kSeeoFieldName;
+    }
+    void _serializeValueToSeeo(BSONObjBuilder* b) const override;
+
+    std::string _keyId;
+};
+
+/// @brief Visitor that does not change a key identifier object.
+class KeyIdConstVisitor {
+public:
+    virtual ~KeyIdConstVisitor() = default;
+    KeyIdConstVisitor() = default;
+    KeyIdConstVisitor(const KeyIdConstVisitor&) = default;
+    KeyIdConstVisitor(KeyIdConstVisitor&&) = default;
+    KeyIdConstVisitor& operator=(const KeyIdConstVisitor&) = default;
+    KeyIdConstVisitor& operator=(KeyIdConstVisitor&&) = default;
+
+    virtual void visit(const KeyFilePath& p) = 0;
+    virtual void visit(const VaultSecretId& id) = 0;
+    virtual void visit(const KmipKeyId& id) = 0;
+};
+
+/// @brief Collection of key identifiers which are used during
+/// the construction of the `WiredTigerKVEngine` class.
+///
+/// @note Ideally, key identifiers should be passed between
+/// source code components as function arguments and return values.
+/// Unfortunately, that would further complicate the already problematic
+/// process of syncing some code components with the upstream repository.
+/// For instance, the `WiredTigerKVEngine` class constructor already has
+/// _ten_ arguments and adding even more is not an option. Thus, having to
+/// use some analog of a nasty global variable for now.
+///
+/// @todo Refactor the code so that this singleton is eliminated.
+class WtKeyIds {
+public:
+    static WtKeyIds& instance();
+
+    /// @brief The present configured key identifier, if any.
+    ///
+    /// Is is read from the storage engine metadata, specifically form the
+    /// storage engine encryption options.
+    std::unique_ptr<KeyId> configured;
+
+    /// @brief The identifier of the key the encryption key database is
+    /// decrypted with.
+    ///
+    /// It may either be equal to configured key identifier or read from the
+    /// mongod configuration.
+    std::unique_ptr<KeyId> decryption;
+
+    /// @brief The future configured key identifier, if any.
+    ///
+    /// It may either be read from the mongod configuration or initialized
+    /// during master key generation and subsequent saving to the key
+    /// management facility.
+    /// It is meant to be saved to the storage engine metadata (specifically,
+    /// to storage engine encryption options) if differs from the present
+    /// configured key identifier.
+    std::unique_ptr<KeyId> futureConfigured;
+
+private:
+    ~WtKeyIds() = default;
+    WtKeyIds() = default;
+    WtKeyIds(const WtKeyIds&) = delete;
+    WtKeyIds(WtKeyIds&&) = delete;
+    WtKeyIds& operator=(const WtKeyIds&) = delete;
+    WtKeyIds& operator=(WtKeyIds&&) = delete;
+};
+}  // namespace encryption
+}  // namespace mongo

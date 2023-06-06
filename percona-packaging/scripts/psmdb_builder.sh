499db58e8057068ad2e8a57177a6bb2235fe9b2b
Igor Solodovnikov <igor.solodovnikov@percona.com>
2023-06-06 19:58:08 +0100

    PSMDB-1278 Update AWS SDK to version 1.9.379

--- percona-packaging/scripts/psmdb_builder.sh
+++ percona-packaging/scripts/psmdb_builder.sh
@@ -175,7 +175,8 @@ get_sources(){
             cd aws-sdk-cpp
                 git reset --hard
                 git clean -xdf
-                git checkout 1.8.187
+                git checkout 1.9.379
+                git submodule update --init --recursive
                 mkdir build
     cd ../../
     tar --owner=0 --group=0 --exclude=.* -czf ${PRODUCT}-${PSM_VER}-${PSM_RELEASE}.tar.gz ${PRODUCT}-${PSM_VER}-${PSM_RELEASE}
@@ -291,7 +292,8 @@ aws_sdk_build(){
         cd aws-sdk-cpp
             git reset --hard
             git clean -xdf
-            git checkout 1.8.187
+            git checkout 1.9.379
+            git submodule update --init --recursive
             mkdir build
             cd build
             CMAKE_CMD="cmake"
@@ -863,7 +865,8 @@ build_tarball(){
             cd aws-sdk-cpp
             git reset --hard
             git clean -xdf
-            git checkout 1.8.187
+            git checkout 1.9.379
+            git submodule update --init --recursive
             mkdir build
             cd build
             set_compiler

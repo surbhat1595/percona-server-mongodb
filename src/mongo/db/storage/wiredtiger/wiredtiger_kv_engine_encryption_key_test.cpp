4396faeaa6674816eae01462b4be8b20f020cfd4
Konstantin Trushin <konstantin.trushin@percona.com>
2023-12-24 13:34:00 +0400

    PSMDB-1283 Unit test: do minor fixes
    (cherry picked from commit 42e17419d9b6834e7ed0a2ff937c41836f82d030)

--- src/mongo/db/storage/wiredtiger/wiredtiger_kv_engine_encryption_key_test.cpp
+++ src/mongo/db/storage/wiredtiger/wiredtiger_kv_engine_encryption_key_test.cpp
@@ -167,7 +167,7 @@ private:
 
 class FakeReadVaultSecret : public ReadVaultSecret {
 public:
-    explicit FakeReadVaultSecret(FakeVaultServer& server, const VaultSecretId& id)
+    FakeReadVaultSecret(FakeVaultServer& server, const VaultSecretId& id)
         : ReadVaultSecret(id), _server(server) {}
 
 
@@ -181,7 +181,7 @@ private:
 
 class FakeSaveVaultSecret : public SaveVaultSecret {
 public:
-    explicit FakeSaveVaultSecret(FakeVaultServer& server, const std::string& secretPath)
+    FakeSaveVaultSecret(FakeVaultServer& server, const std::string& secretPath)
         : SaveVaultSecret(secretPath), _server(server) {}
 
     std::unique_ptr<KeyId> operator()(const Key& key) const override {
@@ -834,9 +834,9 @@ TEST_F(WiredTigerKVEngineEncryptionKeyKmipTest, RotationErrorIfProvidedKeyIdEqua
         "is equal to that the system is already configured with");
 }
 
-#undef ASSERT_CREATE_ENGINE_THROWS_WHAT
-#undef ASSERT_CREATE_ENGINE_THROWS_REASON_CONTAINS
 #undef ASSERT_CREATE_ENGINE_THROWS_REASON_REGEX
+#undef ASSERT_CREATE_ENGINE_THROWS_REASON_CONTAINS
+#undef ASSERT_CREATE_ENGINE_THROWS_WHAT
 
 }  // namespace
 }  // namespace mongo

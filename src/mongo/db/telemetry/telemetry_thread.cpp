93f6d70fe585cb738e44c68d8ef3534397412b15
Igor Solodovnikov <igor.solodovnikov@percona.com>
2024-03-04 20:30:45 +0000

    TEL-6 Split 'cluster_role' into 'config_svr' and 'shard_svr' metrics

--- src/mongo/db/telemetry/telemetry_thread.cpp
+++ src/mongo/db/telemetry/telemetry_thread.cpp
@@ -89,9 +89,6 @@ constexpr StringData kScheduledAt = "scheduledAt"_sd;
 
 constexpr StringData kFalse = "false"_sd;
 constexpr StringData kTrue = "true"_sd;
-constexpr StringData kShardSvr = "shardsvr"_sd;
-constexpr StringData kConfigSvr = "configsvr"_sd;
-constexpr StringData kNoneSvr = "none"_sd;
 
 // names of the fields in the metric file
 constexpr StringData kDbInstanceId = "db_instance_id"_sd;
@@ -102,7 +99,8 @@ constexpr StringData kStorageEngine = "storage_engine"_sd;
 constexpr StringData kReplicaSetId = "db_replication_id"_sd;
 constexpr StringData kReplMemberState = "replication_state"_sd;
 constexpr StringData kClusterId = "db_cluster_id"_sd;
-constexpr StringData kClusterRole = "cluster_role"_sd;
+constexpr StringData kShardSvr = "shard_svr"_sd;
+constexpr StringData kConfigSvr = "config_svr"_sd;
 constexpr StringData kUptime = "uptime"_sd;
 constexpr StringData kSource = "source"_sd;
 
@@ -122,15 +120,6 @@ boost::filesystem::path sdPath(StringData sd) {
 // auxiliary function
 constexpr StringData boolName(bool v) {
     return v ? kTrue : kFalse;
-}
-
-// auxiliary function
-StringData clusterRoleName(mongo::ClusterRole v) {
-    if (v == ClusterRole::ConfigServer)
-        return kConfigSvr;
-    if (v == ClusterRole::ShardServer)
-        return kShardSvr;
-    return kNoneSvr;
 }
 
 bool str_ends_with(const std::string& s, const std::string& sfx);
@@ -352,7 +341,10 @@ private:
             bson.append(kSource, "mongod"_sd);
             if (clusterId.isSet()) {
                 bson.append(kClusterId, clusterId.toString());
-                bson.append(kClusterRole, clusterRoleName(serverGlobalParams.clusterRole));
+                bson.append(kShardSvr,
+                            boolName(serverGlobalParams.clusterRole == ClusterRole::ShardServer));
+                bson.append(kConfigSvr,
+                            boolName(serverGlobalParams.clusterRole == ClusterRole::ConfigServer));
             }
             _prefix = bson.obj();
         }

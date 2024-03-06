8f9acd6efe80c48abb5654c1633e26667675aa58
Igor Solodovnikov <igor.solodovnikov@percona.com>
2024-03-06 01:51:41 +0000

    TEL-6 mongos telemetry implementation

--- src/mongo/db/telemetry/telemetry_thread_base.h
+++ src/mongo/db/telemetry/telemetry_thread_base.h
@@ -31,12 +31,16 @@ Copyright (C) 2024-present Percona and/or its affiliates. All rights reserved.
 
 #pragma once
 
+#include <boost/filesystem.hpp>  // IWYU pragma: keep
 #include <memory>
 #include <string>
 
 #include "mongo/base/status.h"
+#include "mongo/base/string_data.h"
 #include "mongo/bson/bsonobj.h"
+#include "mongo/bson/bsonobjbuilder.h"
 #include "mongo/bson/oid.h"
+#include "mongo/db/operation_context.h"
 #include "mongo/db/service_context.h"
 #include "mongo/platform/atomic_word.h"
 #include "mongo/platform/mutex.h"
@@ -64,6 +68,53 @@ public:
     void run() final;
     void shutdown();
 
+protected:
+    static boost::filesystem::path sdPath(StringData sd);
+    static StringData boolName(bool v);
+
+    // methods called from _initParameters
+    virtual StringData _sourceName() = 0;
+    virtual Status _initInstanceId(const OID& initialId, BSONObjBuilder* pfx) = 0;
+    virtual Status _initDbId(ServiceContext* serviceContext,
+                             OperationContext* opCtx,
+                             const OID& initalId,
+                             BSONObjBuilder* pfx) = 0;
+    virtual Status _initClusterId(ServiceContext* serviceContext,
+                                  OperationContext* opCtx,
+                                  BSONObjBuilder* pfx) = 0;
+
+    // methods called from _advance
+    virtual Status _advancePersist(ServiceContext* serviceContext) = 0;
+
+    // methods called from _writeMetrics
+    virtual Status _appendMetrics(ServiceContext* serviceContext, BSONObjBuilder* builder) = 0;
+
+    // names of the fields in the metric file
+    static constexpr StringData kDbInstanceId = "db_instance_id"_sd;
+    static constexpr StringData kDbInternalId = "db_internal_id"_sd;
+    static constexpr StringData kPillarVersion = "pillar_version"_sd;
+    static constexpr StringData kProFeatures = "pro_features"_sd;
+    static constexpr StringData kStorageEngine = "storage_engine"_sd;
+    static constexpr StringData kReplicaSetId = "db_replication_id"_sd;
+    static constexpr StringData kReplMemberState = "replication_state"_sd;
+    static constexpr StringData kClusterId = "db_cluster_id"_sd;
+    static constexpr StringData kShardSvr = "shard_svr"_sd;
+    static constexpr StringData kConfigSvr = "config_svr"_sd;
+    static constexpr StringData kUptime = "uptime"_sd;
+    static constexpr StringData kSource = "source"_sd;
+
+    // instance id stored in kTelemetryFileName
+    OID _instid;
+
+    // nextScarpe is set to "now + grace" in the constructor
+    // but it is overwritten if we read scheduled time from kTelemetryNamespace
+    Date_t _nextScrape;
+
+    // database id stored as kTelemetryNamespace._id
+    OID _dbid;
+    // constant prefix for each metrics file
+    BSONObj _prefix;
+
 private:
     Status _initParameters(ServiceContext* serviceContext);
     Status _advance(ServiceContext* serviceContext);
@@ -80,18 +131,6 @@ private:
     // The telemetry thread idles on this condition variable for a particular time duration
     // between creating metrics files. It can be triggered early to expediate shutdown.
     stdx::condition_variable _condvar;
-
-    // instance id stored in kTelemetryFileName
-    OID _instid;
-
-    // nextScarpe is set to "now + grace" in the constructor
-    // but it is overwritten if we read scheduled time from kTelemetryNamespace
-    Date_t _nextScrape;
-
-    // database id stored as kTelemetryNamespace._id
-    OID _dbid;
-    // constant prefix for each metrics file
-    BSONObj _prefix;
 };
 
 void initPerconaTelemetryInternal(ServiceContext* serviceContext);

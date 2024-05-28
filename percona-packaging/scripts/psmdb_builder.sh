9d8258e656e427ecec624bbf0957592cc9807278
Sandra <sandra.romanchenko@percona.com>
2024-05-28 15:53:21 +0300

    PSMDB-1462. Update go to 1.22.3 to prevent CVE-2023-39325,CVE-2023-45283,CVE-2023-45287,CVE-2023-45288

--- percona-packaging/scripts/psmdb_builder.sh
+++ percona-packaging/scripts/psmdb_builder.sh
@@ -240,11 +240,11 @@ install_golang() {
     elif [ x"$ARCH" = "xaarch64" ]; then
       GO_ARCH="arm64"
     fi
-    wget https://golang.org/dl/go1.19.12.linux-${GO_ARCH}.tar.gz -O /tmp/golang1.19.tar.gz
-    tar --transform=s,go,go1.19, -zxf /tmp/golang1.19.tar.gz
-    rm -rf /usr/local/go1.19 /usr/local/go1.11  /usr/local/go1.8 /usr/local/go1.9 /usr/local/go1.9.2 /usr/local/go
-    mv go1.19 /usr/local/
-    ln -s /usr/local/go1.19 /usr/local/go
+    wget https://golang.org/dl/go1.22.3.linux-${GO_ARCH}.tar.gz -O /tmp/golang1.22.tar.gz
+    tar --transform=s,go,go1.22, -zxf /tmp/golang1.22.tar.gz
+    rm -rf /usr/local/go1.22 /usr/local/go1.19 /usr/local/go1.11 /usr/local/go1.8 /usr/local/go1.9 /usr/local/go1.9.2 /usr/local/go
+    mv go1.22 /usr/local/
+    ln -s /usr/local/go1.22 /usr/local/go
 }
 
 install_gcc_centos(){
